0f26099ca048bbc3f2a1641950acc00c24ca40f3
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2024-01-24 21:20:19 +0000

    Update code in exercise.py file

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -25,3 +25,26 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """
+        Get the value of a key and optional conversion of the value
+        """
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """
+        Get the value of a key and convert it to a string
+        """
+        value = self.get(key, fn=lambda x: x.decode("utf-8"))
+        return value
+
+    def get_int(self, key: str) -> int:
+        """
+        Get the value of a key and convert it to an integer
+        """
+        value = self.get(key, fn=int)
+        return value
